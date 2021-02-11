8e053adc8f59a3e90062a44dafd55f0c896ce24d
Diana Rukavishnikova <psihomatika@gmail.com>
2021-02-11 10:28:17 +0300

    сверстан header

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [{ // тут описываются правила
+            test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+            use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
+            exclude: /node_modules/ // исключает папку node_modules НУЖНА
+            },
+            {
+            test: /\.css$/i,
+            use: [
+                (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                'css-loader', 
+                'postcss-loader'
+                ]
+            },
+            {
+            test: /\.(png|jpg|gif|ico|svg)$/,
+            use: [
+                'file-loader?name=./images/[name].[ext]', // указали папку, куда складывать изображения
+                {
+                    loader: 'image-webpack-loader',
+                    options: {}
+                },
+            ],
+            },
+            {
+            test: /\.(eot|ttf|woff|woff2)$/,
+            use: {
+                loader: 'file-loader?name=./fonts/[name].[ext]',
+            },
+            }
+            
+        ]
+    },
+    plugins: [ 
+        new MiniCssExtractPlugin({
+            filename: 'index.[contenthash].css',
+          }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html',
+          }),
+        new WebpackMd5Hash(),
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+          }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+              preset: ['default'],
+            },
+            canPrint: true,
+          }),
+    ]
+}
