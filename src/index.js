545a23f671b841b19c18fdcb4ec5396573b3b82c
Diana Rukavishnikova <psihomatika@gmail.com>
2021-02-13 21:31:12 +0300

    адаптирован веся страница

--- src/index.js
+++ src/index.js
@@ -1,44 +1,18 @@
-import "./pages/index.css";
-import Api from './api.js';
-import CardList from './CardList.js';
-import {popupPlace2} from './PopupPlace.js';
-import {popupProfile2} from './PopupProfile.js'
-import {userInfoName, userInfoJob, placesList} from './var.js';
-import {cardContainer} from './CardList.js'
+//import "./pages/index.css";
 
-const keyToken = 'b6e8c1d3-d314-46fd-9a4c-f95ba3fc422a';
-const serverUrl = NODE_ENV === 'development' ?  'http://nomoreparties.co/cohort8/' : 'https://nomoreparties.co/cohort8/';
-//'http://praktikum.tk/cohort8/' : 'https://praktikum.tk/cohort8/';
-const newUserName = document.querySelector('#newUserName'); //первое поле формы Profile (имя)
-const aboutMe = document.querySelector('#aboutMe'); //второе поле формы Profile (информация о себе)
+const menu =  document.querySelector('.header__menu');
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.popup__button');
 
-//здесь был класс апи
+console.log(`${menu}`);
 
-//Используется класс апи
-const api = new Api({//запрос с сервера Заменила 'http://95.216.175.5/cohort3' на serverUrl
-  baseUrl: serverUrl,
-  headers: {
-    authorization: keyToken,
-    'Content-Type': 'application/json'
-  }
-})
-export {api};
-api.getInitialCards().then(res => {
-  cardContainer.addCards(res);
-})
+function open(){
+  popup.classList.add('popup_open');
+}
 
-api.getUserInformation().then((result) => {
-  userInfoName.textContent = result.name
-  userInfoJob.textContent = result.about
-  document.querySelector('.user-info__photo').style.backgroundImage = result.avatar
-  newUserName.value = result.name
-  aboutMe.value = result.about
-})
+function close(){
+  popup.classList.remove('popup_open');
+}
 
-
-//здесь был класс кард, создающий карточку
-//здесь был класс кардлист, для хранения и отрисовки карточек
-//здесь был класс попап для открытия всплывающего окна
-
-
-//попап увеличения картинки
+menu.addEventListener('click', open);
+closeButton.addEventListener('click', close);
